a7ac8edff9a1c1dddb48a03087012442d43f7527
Viktor <klochkov44@gmail.com>
2025-06-26 13:33:05 +0200

    week1, prime factors

--- code/starter_kits/prime_factors_sk/prime_factors.h
+++ code/starter_kits/prime_factors_sk/prime_factors.h
@@ -1,7 +1,49 @@
 // Copyright (c) 2024 Dr. Matthias Hölzl. All rights reserved.
 
 #pragma once
+
+#include <iostream>
 #include <vector>
 
-namespace prime_factors_sk
-{}
+namespace prime_factors_sk {
+  /**
+   * Computes the prime factors for the specified number.
+   *
+   * This function identifies and returns all prime factors of the given integer.
+   *
+   * @param number The integer number for which prime factors are to be computed.
+   *               It should be a positive number greater than or equal to 1.
+   * @return A vector containing the prime factors of the input number in ascending order.
+   *         If the number is 1, an empty vector is returned as 1 has no prime factors.
+   */
+  std::vector<long> compute_prime_factors(long number);
+
+  /**
+   * Formats a list of prime numbers and writes them to the specified output stream.
+   *
+   * This function takes a vector of prime numbers and writes them to the provided
+   * output stream. The prime numbers are formatted as a comma-separated string.
+   * An empty vector results in no output being written to the stream.
+   *
+   * @param primes The vector containing prime numbers to be formatted.
+   *               It can be an empty vector, in which case no output is written.
+   * @param os The output stream to which the formatted primes will be written.
+   *           Typically, this would be `std::cout` or a file-based output stream.
+   */
+  void format_primes(const std::vector<long>& primes, std::ostream& os);
+
+  /**
+   * Computes the prime factors of a given number and writes them to the specified output stream.
+   *
+   * This function calculates all prime factors of the provided number, formats them as a comma-separated
+   * string, and outputs the result to the specified stream. If the number is 1 or has no prime factors,
+   * no output is written.
+   *
+   * @param number The positive integer for which prime factors should be computed.
+   *               It should be greater than or equal to 1.
+   * @param os The output stream where the formatted prime factors will be written.
+   *           Defaults to `std::cout` if no stream is provided.
+   */
+  void compute_and_write_prime(long number, std::ostream &os = std::cout);
+
+}
--- code/starter_kits/prime_factors_sk/prime_factors_test.cpp
+++ code/starter_kits/prime_factors_sk/prime_factors_test.cpp
@@ -1,12 +1,86 @@
 // Copyright (c) 2024 Dr. Matthias Hölzl. All rights reserved.
 
 #include "prime_factors.h"
+#include "../../completed/prime_factors/prime_factors.h"
 
 #include <catch2/catch_test_macros.hpp>
 
+#include <sstream>
+
 using namespace prime_factors_sk;
 
-TEST_CASE ("Failing test")
+TEST_CASE ("compute_prime_factors()")
 {
-    REQUIRE(false);
+  SECTION("Compute prime factors of 1 return []") {
+    std::vector<long> expected{};
+    CHECK(compute_prime_factors(1) == expected);
+  }
+
+  SECTION("Compute prime factors of 2 return [2]") {
+    std::vector expected{2L};
+    CHECK(compute_prime_factors(2L) == expected);
+  }
+
+  SECTION("Compute prime factors of 3 return [3]") {
+    std::vector expected{3L};
+    CHECK(compute_prime_factors(3L) == expected);
+  }
+
+  SECTION("Compute prime factors of 4 return [2, 2]") {
+    std::vector expected{2L, 2L};
+    CHECK(compute_prime_factors(4L) == expected);
+  }
+
+  SECTION("Compute prime factors of 5 return [5]") {
+    std::vector expected{5L};
+    CHECK(compute_prime_factors(5L) == expected);
+  }
+
+  SECTION("Compute prime factors of 6 return [2, 3]") {
+    std::vector expected{2L, 3L};
+    CHECK(compute_prime_factors(6L) == expected);
+  }
+}
+
+TEST_CASE("Format primes") {
+  SECTION("formats an empty vector correctly") {
+    std::vector<long> primes{};
+    std::string expected{""};
+    std::stringstream os;
+    format_primes(primes, os);
+    CHECK(os.str() == expected);
+  }
+
+  SECTION("formats a vector with one element correctly") {
+    std::vector primes{2L};
+    std::string expected{"2"};
+    std::stringstream os;
+    format_primes(primes, os);
+    CHECK(os.str() == expected);
+  }
+
+  SECTION("formats a vector with two elements correctly") {
+    std::vector primes{2L, 3L};
+    std::string expected{"2, 3"};
+    std::stringstream os;
+    format_primes(primes, os);
+    CHECK(os.str() == expected);
+  }
+
+  SECTION("formats a vector with 5 elements correctly") {
+    std::vector primes{2L, 3L, 5L, 7L, 11L};
+    std::string expected{"2, 3, 5, 7, 11"};
+    std::stringstream os;
+    format_primes(primes, os);
+    CHECK(os.str() == expected);
+  }
+}
+
+TEST_CASE("Compute and write prime") {
+  SECTION("print the correct output for 100") {
+    std::stringstream os;
+    std::string expected{"The primes of 100 are: 2, 2, 5, 5"};
+    compute_and_write_prime(100, os);
+    CHECK(os.str() == expected);
+  }
 }

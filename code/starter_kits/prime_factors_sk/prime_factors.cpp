d3acb98058f4fe5bc18f132550db75de26bb6eb8
Viktor <klochkov44@gmail.com>
2025-06-26 13:33:05 +0200

    week1, prime factors

--- code/starter_kits/prime_factors_sk/prime_factors.cpp
+++ code/starter_kits/prime_factors_sk/prime_factors.cpp
@@ -1,3 +1,36 @@
 // Copyright (c) 2024 Dr. Matthias Hölzl. All rights reserved.
 
 #include "prime_factors.h"
+
+#include <iostream>
+#include <sstream>
+
+namespace prime_factors_sk {
+
+  std::vector<long> compute_prime_factors(long number) {
+    std::vector<long> result;
+
+    for (long factor{2}; factor <= number; factor++) {
+      while (number % factor == 0) {
+        result.push_back(factor);
+        number /= factor;
+      }
+    }
+    return result;
+  }
+
+  void format_primes(const std::vector<long> &primes, std::ostream &os) {
+    std::string prefix{""};
+    for (auto& prime : primes) {
+      os << prefix << prime;
+      prefix = ", ";
+    }
+  }
+
+  void compute_and_write_prime(long number, std::ostream &os) {
+    const auto primes = compute_prime_factors(number);
+    os << "The primes of " << number << " are: ";
+    format_primes(primes, os);
+  }
+
+}
